d639f6b9b09485db20e2c205986a8bccf731cf50
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-10 16:59:28 +0300

    update twitter.com

--- app/page.tsx
+++ app/page.tsx
@@ -117,9 +117,6 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          
-          </p>
         </a>
       </div>
     </main>

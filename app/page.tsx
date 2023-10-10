873b0e532a4d1d498ad334e4a7bc037f276cb7e0
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-10 17:02:34 +0300

    add LinkedIn

--- app/page.tsx
+++ app/page.tsx
@@ -119,6 +119,21 @@ export default function Home() {
           </h2>
         </a>
       </div>
+      <a
+          href="https://linkedin.com/in/dominikkugelmann"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            LinkedIn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </a>
+
+        
     </main>
   )
 }

a6123f7efe2350c3f244b58d9ea6b6aedd9314c9
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-10 21:00:25 +0300

    remove duplicate

--- app/page.tsx
+++ app/page.tsx
@@ -147,20 +147,6 @@ export default function Home() {
             </span>
           </h2>
         </a>
-
-        <a
-          href="https://instagram.com/da_allgeier"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Instagram{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
       </div>
     </main>
   )

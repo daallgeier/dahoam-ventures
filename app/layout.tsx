8ea6be0d7f89360bd00aef92c3220020be2b1241
Tangerine Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-10-16 14:58:25 +0300

    change favicon and add robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,31 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'dahoam ventures',
-  description: 'dahoam ventures by tangerine',
+  title: {
+    default: 'dahoam ventures',
+    template: '%s | dahoam ventures',
+  },
+  description: {
+    'dahoam ventures by tangerine',
+  },
+  icons: {
+    shortcut: '/public/dv-nonlooping.gif',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+    GPTbot: {
+      Allow: '/',
+      Disallow: '/*',
+    }
+  },
 }
 
 export default function RootLayout({
